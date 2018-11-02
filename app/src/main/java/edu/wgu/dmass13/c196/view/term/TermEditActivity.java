b7d3cead15d7a7bbd5a6c9d06873bd4c59ac4ebd
Wayne Massey <wmassey76@gmail.com>
2018-11-02 17:43:12 -0400

    ...

--- app/src/main/java/edu/wgu/dmass13/c196/view/term/TermEditActivity.java
+++ app/src/main/java/edu/wgu/dmass13/c196/view/term/TermEditActivity.java
@@ -1,4 +1,15 @@
 package edu.wgu.dmass13.c196.view.term;
 
-public class TermEditActivity {
+import android.app.Activity;
+import android.os.Bundle;
+
+import edu.wgu.dmass13.c196.R;
+
+public class TermEditActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mentor_edit);
+    }
 }
